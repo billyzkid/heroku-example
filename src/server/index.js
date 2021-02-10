58371d97bd79ca60aaf5d15a9fab5eeddbd047a3
Will Allan <will.allan@disneystreaming.com>
2021-02-09 18:55:22 -0800

    Reorganization

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '../client')));
 
 io.on('connection', function(socket) {
   console.log('new connection from ' + socket.id);
